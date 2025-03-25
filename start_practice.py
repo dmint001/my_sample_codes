107c88e0e02b93a61b9b89b96840c3345dbd03ef
Deb Mukherjee <83813696+dmint001@users.noreply.github.com>
2025-03-25 11:52:06 +0530

    Update start_practice.py

--- start_practice.py
+++ start_practice.py
@@ -1,4 +1,4 @@
-## Count words in a sentence
+## ###########3Count words in a sentence
 sentence = "These days code is a necessity. Let's learn to code."
 sentence = list(sentence.replace(".","").split())
 items = {}
@@ -12,8 +12,9 @@ def checkExist(item):
 list(map(checkExist,sentence))
 print(items)
 
-#just keep non repeating chars in a string
+#############just keep non repeating chars in a string
 
+####approach 1
 sentence = "I am a string. I am very useful"
 sentence = list(sentence.replace(".",""))
 charlist = []
@@ -22,3 +23,20 @@ for item in sentence:
       charlist.append(item)
 charlist.remove(" ")
 charlist
+
+#### approach 2
+sentence = "I am a string. I am very useful"
+sentence = set(sentence.replace(".",""))
+sentence.remove(" ")
+sentence
+
+#find missing digits in range
+import random
+hi = int(input("Enter highest"))
+rng = int(input("Enter a range"))
+smallSet = set(random.sample(range(1, hi), rng))
+print(smallSet)
+fullSet = set(range(1,hi + 1))
+print(fullSet)
+missing = fullSet.difference(smallSet)
+print(missing)
